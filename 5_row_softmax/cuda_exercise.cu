991e397dffafd4757d4588572fc01727337abb14
zhhsplendid <zhhsplendid@gmail.com>
2025-08-13 09:13:08 -0700

    A commit that can run

--- 5_row_softmax/cuda_exercise.cu
+++ 5_row_softmax/cuda_exercise.cu
@@ -7,92 +7,114 @@
 #include <cuda_runtime.h>
 
 // softmax on tensor of size [ROW, COL]
-// where ROW stands for batch size, we do softmax on COL 
-const int ROW = 24;
-const int COL = 256;
+// where ROW stands for batch size, we do softmax on COL
+const int ROW = 4096;
+const int COL = 1024;
 
 const int SHARED_SIZE = 1024;
-const int NUM_THREADS = 256;
 
-const int OP_REDUCE_SUM = 0;
-const int OP_REDUCE_MAX = 1;
-// OpCode = 0 -> ReduceSum
-// otherwise -> ReduceMax
-// Store result in values[0], tid must be greater than len
-template <typename T, int OpCode>
-__device__ void warpReduce(T *values, int len) {
-  int tid = threadIdx.x;
-  int warp_size = 32;
-  for (int step = 1; step < len; step *= warp_size) {
-    if (tid * step < len) {
-      unsigned mask = __ballot_sync(0xffffffff, tid * step < len);
-      T val = values[tid];
-      for (int offset = warp_size >> 1; offset > 0; offset >>= 1) {
-        val = OpCode == OP_REDUCE_SUM ?
-              val + __shfl_down_sync(mask, val, offset, warp_size):
-              max(val, __shfl_down_sync(mask, val, offset, warp_size)); 
-      }
-      __syncwarp();
-      int next_step = step * warp_size;
-      if (tid % next_step == 0) {
-        values[tid / next_step] = val;
-      }
-      __syncwarp();
-    }
+template <typename T> __device__ T warpReduceSum(T val) {
+  for (int offset = 16; offset > 0; offset >>= 1) {
+    val += __shfl_down_sync(0xffffffff, val, offset);
   }
+  return val;
 }
 
-
-template <typename T, int OpCode>
-__device__ T blockReduce(const T *values, int len) {
-  __shared__ T smem[SHARED_SIZE];
-  int NUM = (len + SHARED_SIZE - 1) / SHARED_SIZE;
-  T ans = 0;
-  int tid = threadIdx.x;
-  for (int i = 0; i < NUM; ++i) {
-    if (i * SHARED_SIZE + tid < len && tid < SHARED_SIZE) {
-      smem[tid] = values[i * SHARED_SIZE + tid];
-    }
-    int rem_len = min((len - i * SHARED_SIZE), SHARED_SIZE);
-      
-    __syncthreads();
-    warpReduce<T, OpCode>(smem, rem_len); 
-    if (tid == 0) {
-      printf("r = %d, smem[0] = %f\n", blockIdx.x, smem[0]);
-    }
-    __syncthreads();
-    ans += smem[0];
+template <typename T> __device__ T warpReduceMax(T val) {
+  for (int offset = 16; offset > 0; offset >>= 1) {
+    val = max(val, __shfl_down_sync(0xffffffff, val, offset));
   }
-  return ans;
+  return val;
 }
 
 template <typename T>
 __global__ void softmaxKernel(const T *__restrict__ X, T *__restrict__ Out,
                               int row, int col) {
+  __shared__ T smem[SHARED_SIZE];
   int col_begin = blockIdx.x * col;
-  T x_max = blockReduce<T, OP_REDUCE_MAX>(X + col_begin, col);
+  const T *input_row = X + col_begin;
+  T *output_row = Out + col_begin;
+
+  T local_max = -INFINITY;
+  T local_norm = 0.0;
+
+  int warp_size = 32;
   int tid = threadIdx.x;
-  int pos = col_begin + tid;
-
-  if (tid < col) {
-    if (tid == 0) {
-      printf("cuda r = %d x_max = %f\n", blockIdx.x, x_max);
+  // Handle column size larger than thread num
+  for (int i = tid; i < col; i += blockDim.x) {
+    T elem = input_row[i];
+    if (elem > local_max) {
+      local_max = elem;
+      local_norm *= exp(local_max - elem);
     }
-    Out[pos] = exp(X[pos] - x_max);
-    __syncthreads();
-    //printf("tid = %d, Out[pos] = %f\n", tid, Out[pos]);
-    T e_sum = blockReduce<T, OP_REDUCE_SUM>(Out + col_begin, col);
-    if (tid == 0) {
-      printf("cuda r = %d e_sum = %f\n", blockIdx.x, e_sum);
+    local_norm += exp(elem - local_max);
+  }
+  __syncthreads();
+
+  T val = local_max;
+  //printf("tid = %d, val = %f\n", tid, val);
+  val = warpReduceMax<T>(val);
+  //printf("tid = %d, after reduce val = %f\n", tid, val);
+
+  if (blockDim.x > warp_size) {
+    if (tid % warp_size == 0) {
+      smem[tid / warp_size] = val;
     }
     __syncthreads();
 
-    Out[pos] = Out[pos] / e_sum;
+    if (tid < warp_size) {
+      val = tid < blockDim.x / warp_size ? smem[tid] : -INFINITY;
+      val = warpReduceMax<T>(val);
+      if (tid == 0) {
+        smem[0] = val;
+      }
+    }
+  } else {
+    if (tid == 0) {
+      smem[0] = val;
+    }
+  }
+  __syncthreads();
+
+  T x_max = smem[0];
+  
+  val = local_norm * exp(local_max - x_max);
+  val = warpReduceSum(val);
+
+  if (blockDim.x > warp_size) {
+    if (tid % warp_size == 0) {
+      smem[tid / warp_size] = val;
+    }
+    __syncthreads();
+
+    if (tid < warp_size) {
+      val = tid < blockDim.x / warp_size ? smem[tid] : 0.0f;
+      val = warpReduceSum(val);
+      if (tid == 0) {
+        smem[0] = val;
+      }
+    }
+  } else {
+    if (tid == 0) {
+      smem[0] = val;
+    }
+  }
+  __syncthreads();
+
+  T e_sum = smem[0];
+  //if (tid == 0) {
+  //  printf("cuda r = %d x_max = %f\n", blockIdx.x, x_max);
+  //  printf("cuda r = %d e_sum = %f\n", blockIdx.x, e_sum);
+  //}
+
+  for (int i = tid; i < col; i += blockDim.x) {
+    output_row[i] = exp(input_row[i] - x_max) / e_sum;
   }
 }
 
 template <typename T>
-void cudaSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row, int col) {
+void cudaSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row,
+                 int col) {
   T *dev_X;
   T *dev_Out;
   int M = row * col;
@@ -106,7 +128,8 @@ void cudaSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row, int col)
   cudaEventRecord(start);
 
   // each block run a row
-  softmaxKernel<T><<<row, NUM_THREADS>>>(dev_X, dev_Out, row, col);
+  int num_threads = min(1024, col);
+  softmaxKernel<T><<<row, num_threads>>>(dev_X, dev_Out, row, col);
 
   cudaEventRecord(end);
   cudaEventSynchronize(end);
@@ -125,7 +148,8 @@ void cudaSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row, int col)
 
 // Out = Softmax(X) where the length is M
 template <typename T>
-void cpuSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row, int col) {
+void cpuSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row,
+                int col) {
   if (row <= 0 || col <= 0) {
     return;
   }
@@ -137,13 +161,13 @@ void cpuSoftmax(const T *__restrict__ X, T *__restrict__ Out, int row, int col) 
     for (int i = 1; i < col; ++i) {
       x_max = max(X[col_begin + i], x_max);
     }
-    printf("cpu r = %d, x_max = %f\n", r, x_max);
     T e_sum = 0;
     for (int i = 0; i < col; ++i) {
       Out[col_begin + i] = exp(X[col_begin + i] - x_max);
       e_sum += Out[col_begin + i];
     }
-    printf("cpu r = %d, e_sum = %f\n", r, e_sum);
+    // printf("cpu r = %d, x_max = %f\n", r, x_max);
+    // printf("cpu r = %d, e_sum = %f\n", r, e_sum);
 
     for (int i = 0; i < col; ++i) {
       Out[col_begin + i] /= e_sum;
@@ -161,10 +185,10 @@ bool checkEqual(const float *in1, const float *in2, int row, int col) {
     for (int j = 0; j < col; ++j) {
       int pos = i * col + j;
       float diff = abs(in1[pos] - in2[pos]);
-      //printf("diff = %f\n", diff);
-      if ((in1[pos] >= 1e-5 && diff / in1[pos] > 1e-2) || diff > 1e-4) {
-        printf("Answer is False! in1[%d][%d] = %f, in2[%d][%d] = %f\n",
-               i, j, in1[pos], i, j, in2[pos]);
+      // printf("diff = %f\n", diff);
+      if ((in1[pos] >= 1e-5 && diff / in1[pos] > 1e-3) || diff > 1e-4) {
+        printf("Answer is False! in1[%d][%d] = %f, in2[%d][%d] = %f\n", i, j,
+               in1[pos], i, j, in2[pos]);
         return false;
       }
     }
@@ -181,7 +205,7 @@ int main() {
   int M = ROW * COL;
   float *X = new float[M];
   for (int i = 0; i < M; ++i) {
-    X[i] = static_cast<float>((M - i) % 10);
+    X[i] = static_cast<float>((i) % 10);
     //X[i] = dist(gen);
   }
   printf("Random generated Matrix size row = %d, col = %d\n", ROW, COL);
@@ -191,12 +215,15 @@ int main() {
 
   float *cuda_Out = new float[M];
   cudaSoftmax(X, cuda_Out, ROW, COL);
-  
+
   checkEqual(cpu_Out, cuda_Out, ROW, COL);
 
+  /*
   for (int i = 0; i < 3; ++i) {
     printf("%f %f\n", cpu_Out[i], cuda_Out[i]);
   }
+  */
+  delete[] X;
   delete[] cpu_Out;
   delete[] cuda_Out;
 
